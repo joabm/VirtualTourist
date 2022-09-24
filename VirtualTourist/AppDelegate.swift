072f198038d5290f74e9326cf8f83e89352e4d05
joabm <84096469+joabm@users.noreply.github.com>
2022-09-24 00:26:52 -0700

    updated core data stack setup to iOS 13

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
+//    var window: UIWindow?
 
     let dataController = DataController(modelName: "VirtualTourist")
 
@@ -21,9 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         dataController.load()
         
         //configure first view and inject the dataController dependency into MapView
-        let navigationController = window?.rootViewController as! UINavigationController
-        let mapView = navigationController.topViewController as! MapView
-        mapView.dataController = dataController
+//        let navigationController = window?.rootViewController as! UINavigationController
+//        let mapView = navigationController.topViewController as! MapView
+//        mapView.dataController = dataController
         
         return true
     }
@@ -41,7 +42,64 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    // MARK: - Core Data stack
+        
+        lazy var persistentContainer: NSPersistentContainer = {
+            /*
+             The persistent container for the application. This implementation
+             creates and returns a container, having loaded the store for the
+             application to it. This property is optional since there are legitimate
+             error conditions that could cause the creation of the store to fail.
+             */
+            let container = NSPersistentContainer(name: "VirtualTourist")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    
+                    /*
+                     Typical reasons for an error here include:
+                     * The parent directory does not exist, cannot be created, or disallows writing.
+                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                     * The device is out of space.
+                     * The store could not be migrated to the current model version.
+                     Check the error message to determine what the actual problem was.
+                     */
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }()
+        
+        // MARK: - Core Data Saving support
+        
+        func saveContext () {
+            let context = persistentContainer.viewContext
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
+    
 
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        saveViewContext()
+//    }
+//
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        saveViewContext()
+//    }
+//
+//    func saveViewContext(){
+//        try? dataController.viewContext.save()
+//    }
 
 }
 
