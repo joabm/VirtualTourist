ef89008713ec9286aaf634ab64adafea3a5eb627
joabm <84096469+joabm@users.noreply.github.com>
2022-09-26 08:34:36 -0700

    set up collection view elements and folders

--- VirtualTourist/Controller/CollectionView.swift
+++ VirtualTourist/Controller/CollectionView.swift
@@ -7,12 +7,43 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class CollectionView: UIViewController, UICollectionViewDelegate {
     
     // MARK: Properties
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     var selectedPin: Pin!
+    
     var dataController: DataController!
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    // MARK: Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+}
+
+extension CollectionView: MKMapViewDelegate {
+    
+    
+    
     
 }
