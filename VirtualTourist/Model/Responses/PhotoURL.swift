df2ce7db7678d4d156968df2221445d73dcf3253
joabm <84096469+joabm@users.noreply.github.com>
2022-10-10 09:46:42 -0700

    added new struct files

---
+++ VirtualTourist/Model/Responses/PhotoURL.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoURL.swift
+//  VirtualTourist
+//
+//  Created by Joab Maldonado on 10/9/22.
+//
+
+import Foundation
+
+struct PhotoURL: Codable {
+    let id: String
+    let secret: String
+    let server: String
+}
---
+++ VirtualTourist/Model/Responses/Photos.swift
@@ -0,0 +1,13 @@
+//
+//  Photos.swift
+//  VirtualTourist
+//
+//  Created by Joab Maldonado on 9/26/22.
+//
+
+import Foundation
+
+struct Photos: Codable {
+    let photos: LocationPhotos
+}
+
